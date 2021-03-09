fda311b70ab48c77da049435c5248e2f020eace4
Joao Marcos R de Carvalho <joaomarcosribeiroc@gmail.com>
2021-03-08 22:24:13 -0300

    remove version field from AbstractEntity class

--- src/main/java/br/com/supera/game/db/AbstractEntity.java
+++ src/main/java/br/com/supera/game/db/AbstractEntity.java
@@ -12,9 +12,6 @@ public abstract class AbstractEntity {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 
-	@Version
-	private Integer version;
-
 	/* CONSTRUCTORS */
 	public AbstractEntity() {
 		super();
@@ -27,13 +24,5 @@ public abstract class AbstractEntity {
 
 	public void setId(Integer id) {
 		this.id = id;
-	}
-
-	public Integer getVersion() {
-		return version;
-	}
-
-	public void setVersion(Integer version) {
-		this.version = version;
 	}
 }
