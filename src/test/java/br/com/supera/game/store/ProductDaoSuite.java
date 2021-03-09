9f090816082f997bee4de82b0183c2b2b230c217
Joao Marcos R de Carvalho <jc808853@hpeautos.com.br>
2021-03-08 21:20:40 -0300

    add the suite class for the test

---
+++ src/test/java/br/com/supera/game/store/ProductDaoSuite.java
@@ -0,0 +1,14 @@
+package br.com.supera.game.store;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({
+	ProductDaoTeste.class,
+//	ProductDaoTestQueryFormatOFF.class
+	})
+public class ProductDaoSuite {
+
+}
