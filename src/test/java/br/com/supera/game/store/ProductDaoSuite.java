dbcc25031484a3cac7cd55bda528d8a44af44ca8
Joao Marcos R de Carvalho <joaomarcosribeiroc@gmail.com>
2021-03-08 21:53:32 -0300

    fix SuiteClass name at ProductDaoSuite

--- src/test/java/br/com/supera/game/store/ProductDaoSuite.java
+++ src/test/java/br/com/supera/game/store/ProductDaoSuite.java
@@ -6,7 +6,7 @@ import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
 @SuiteClasses({
-	ProductDaoTeste.class,
+	ProductDaoTest.class,
 //	ProductDaoTestQueryFormatOFF.class
 	})
 public class ProductDaoSuite {
