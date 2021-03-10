b727ba716283d16a79c67842be813e0f752fcdfc
Joao Marcos R de Carvalho <joaomarcosribeiroc@gmail.com>
2021-03-10 18:48:32 -0300

    remove dev profile from the buildScript.bat

--- buildScript.bat
+++ buildScript.bat
@@ -19,7 +19,7 @@ call startup.bat
 REM REDEPLOYING
 echo redeploying
 cd C:\Users\jc808853\Dropbox\projectss\Fork - SuperaRH\PS-Java
-call mvn clean package -Pdev
+call mvn clean package 
 
 xcopy "C:\Users\jc808853\Dropbox\projectss\Fork - SuperaRH\PS-Java\target\game-store-1.0.0-SNAPSHOT.war" "C:\Users\jc808853\personal_development\servers\apache-tomcat-9.0.39\webapps\"
 
