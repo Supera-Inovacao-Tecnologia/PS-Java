74dba36bc50ed9acf61f7eacae2d9aa98bb76614
Joao Marcos R de Carvalho <joaomarcosribeiroc@gmail.com>
2021-03-10 18:56:27 -0300

    remove field version and annotation GeneratedValue to conform with the given data

--- src/main/java/br/com/supera/game/db/AbstractEntity.java
+++ src/main/java/br/com/supera/game/db/AbstractEntity.java
@@ -1,15 +1,12 @@
 package br.com.supera.game.db;
 
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.MappedSuperclass;
-import javax.persistence.Version;
 
 @MappedSuperclass
 public abstract class AbstractEntity {
 	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
+//	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 
 	/* CONSTRUCTORS */
