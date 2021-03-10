2c45baa88da9a575a37b14754421a25e6d4307b9
Joao Marcos R de Carvalho <joaomarcosribeiroc@gmail.com>
2021-03-10 20:26:17 -0300

    fix id generation for production test

--- src/main/java/br/com/supera/game/db/AbstractEntity.java
+++ src/main/java/br/com/supera/game/db/AbstractEntity.java
@@ -1,12 +1,14 @@
 package br.com.supera.game.db;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.MappedSuperclass;
 
 @MappedSuperclass
 public abstract class AbstractEntity {
 	@Id
-//	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 
 	/* CONSTRUCTORS */
