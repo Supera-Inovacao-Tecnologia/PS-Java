1fcc768b83c44cc742b38cbd0ef1684e82c08e5a
Joao Marcos R de Carvalho <joaomarcosribeiroc@gmail.com>
2021-03-11 18:21:58 -0300

    improve checkout model

--- src/main/java/br/com/supera/game/store/Checkout.java
+++ src/main/java/br/com/supera/game/store/Checkout.java
@@ -10,50 +10,67 @@ import br.com.supera.game.db.AbstractEntity;
 
 @Entity
 public class Checkout extends AbstractEntity {
-	
+
 	@OneToMany
 	private List<Product> productList;
 	private BigDecimal shipingPrice;
+	private Boolean shippingPriceStatus;
 	private BigDecimal subtotalPrice;
 	private BigDecimal totalPrice;
-	
-	
+
 	/* CONSTRUCTORS */
-	
-	/*It is only possible to get an up to date instance of a cart's checkout */
-	public Checkout(Integer cartId) {
-		super();
-		
-		//Access the up to date cart from the database
-	}
-	
+
 	public Checkout() {
 		super();
+		this.shippingPriceStatus = true; // a priori
 	}
 
+//	/*It is only possible to get an up to date instance of a cart's checkout */
+//	public Checkout(Integer cartId) {
+//		super();
+//		
+//		//Access the up to date cart from the database
+//	}
+//	
+
 	/* GETTERS/SETTERS */
-	
+
 	public List<Product> getProductList() {
 		return productList;
 	}
-	public void setProductList(List<Product> productList) {
-		this.productList = productList;
-	}
+
 	public BigDecimal getShipingPrice() {
 		return shipingPrice;
 	}
-	public void setShipingPrice(BigDecimal shipingPrice) {
-		this.shipingPrice = shipingPrice;
+
+	public Boolean getShippingPriceStatus() {
+		return shippingPriceStatus;
 	}
+
 	public BigDecimal getSubtotalPrice() {
 		return subtotalPrice;
 	}
-	public void setSubtotalPrice(BigDecimal subtotalPrice) {
-		this.subtotalPrice = subtotalPrice;
-	}
+
 	public BigDecimal getTotalPrice() {
 		return totalPrice;
 	}
+
+	public void setProductList(List<Product> productList) {
+		this.productList = productList;
+	}
+
+	public void setShipingPrice(BigDecimal shipingPrice) {
+		this.shipingPrice = shipingPrice;
+	}
+
+	public void setShippingPriceStatus(Boolean shippingPriceStatus) {
+		this.shippingPriceStatus = shippingPriceStatus;
+	}
+
+	public void setSubtotalPrice(BigDecimal subtotalPrice) {
+		this.subtotalPrice = subtotalPrice;
+	}
+
 	public void setTotalPrice(BigDecimal totalPrice) {
 		this.totalPrice = totalPrice;
 	}
