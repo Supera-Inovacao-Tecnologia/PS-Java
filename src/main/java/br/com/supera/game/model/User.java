e1c7b1f37e224efd63bb5eefc7b93a2e5835de97
Joao Marcos R de Carvalho <joaomarcosribeiroc@gmail.com>
2021-03-10 21:29:10 -0300

    add cart to user model

--- src/main/java/br/com/supera/game/model/User.java
+++ src/main/java/br/com/supera/game/model/User.java
@@ -1,70 +1,21 @@
 package br.com.supera.game.model;
 
+import javax.annotation.Generated;
 import javax.persistence.Entity;
 
 import br.com.supera.game.db.AbstractEntity;
-import javax.annotation.Generated;
+import br.com.supera.game.store.Cart;
 
 @Entity
 public class User extends AbstractEntity {
-
+	
 	private String name;
 	
 	private String lastName;
-	
+
 	private String email;
 
-	@Generated("SparkTools")
-	private User(Builder builder) {
-		this.name = builder.name;
-		this.lastName = builder.lastName;
-		this.email = builder.email;
-	}
-	
-	public User() {
-		super();
-	}
-
-
-	public String getName() {
-		return name;
-	}
-
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-
-	public String getLastName() {
-		return lastName;
-	}
-
-
-	public void setLastName(String lastName) {
-		this.lastName = lastName;
-	}
-
-
-	public String getEmail() {
-		return email;
-	}
-
-
-	public void setEmail(String email) {
-		this.email = email;
-	}
-
-
-	/**
-	 * Creates builder to build {@link User}.
-	 * @return created builder
-	 */
-	@Generated("SparkTools")
-	public static Builder builder() {
-		return new Builder();
-	}
-
+	private Cart cart;
 
 	/**
 	 * Builder to build {@link User}.
@@ -78,8 +29,12 @@ public class User extends AbstractEntity {
 		private Builder() {
 		}
 
-		public Builder withName(String name) {
-			this.name = name;
+		public User build() {
+			return new User(this);
+		}
+
+		public Builder withEmail(String email) {
+			this.email = email;
 			return this;
 		}
 
@@ -88,14 +43,70 @@ public class User extends AbstractEntity {
 			return this;
 		}
 
-		public Builder withEmail(String email) {
-			this.email = email;
+		public Builder withName(String name) {
+			this.name = name;
 			return this;
 		}
+	}
+	
+	/**
+	 * Creates builder to build {@link User}.
+	 * @return created builder
+	 */
+	@Generated("SparkTools")
+	public static Builder builder() {
+		return new Builder();
+	}
 
-		public User build() {
-			return new User(this);
-		}
+	public User() {
+		super();
+	}
+	
+	@Generated("SparkTools")
+	private User(Builder builder) {
+		this.name = builder.name;
+		this.lastName = builder.lastName;
+		this.email = builder.email;
+	}
+
+
+	public Cart getCart() {
+		return cart;
+	}
+
+
+	public String getEmail() {
+		return email;
+	}
+
+
+	public String getLastName() {
+		return lastName;
+	}
+
+
+	public String getName() {
+		return name;
+	}
+
+
+	public void setCart(Cart cart) {
+		this.cart = cart;
+	}
+
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+
+	public void setName(String name) {
+		this.name = name;
 	}
 	
 	
